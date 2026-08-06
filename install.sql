d57af273427ba1064d1a962a73fe35c3872bc26a
hanys66 <hanyshaker@hotmail.com>
2026-08-06 22:56:34 +0300

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,1 +1,8 @@
+PROMPT ======================================
+PROMPT HRM_PRO Installation
+PROMPT ======================================
 
+SET DEFINE OFF
+SET SERVEROUTPUT ON
+
+PROMPT Installation Started...
