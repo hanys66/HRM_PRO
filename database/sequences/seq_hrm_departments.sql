789d80a840c7850a82d692c2adae3f1966608c27
hanys66 <hanyshaker@hotmail.com>
2026-08-10 21:13:59 +0300

    Update seq_hrm_departments.sql

--- database/sequences/seq_hrm_departments.sql
+++ database/sequences/seq_hrm_departments.sql
@@ -1,3 +1,16 @@
+/*==============================================================================
+ Project    : HRM_PRO
+ File       : seq_hrm_departments.sql
+ Object     : SEQ_HRM_DEPARTMENTS
+ Author     : Hany Shaker
+ Created    : 2026-08-10
+ Version    : 1.0
+
+ Description:
+     Generates primary key values for HRM_DEPARTMENTS.
+
+==============================================================================*/
+
 CREATE SEQUENCE SEQ_HRM_DEPARTMENTS
     START WITH 1
     INCREMENT BY 1
